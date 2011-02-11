b62662d2d3972c0ccc75ddc86feccfdcd84a8bc4
Federico Holgado <fholgado@gmail.com>
2011-02-10 22:38:02 -0500

    Fixed matching bug

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
       var post_link = encodeURIComponent($('a.permalink', this).attr('href'));
       var post_author = encodeURIComponent($('div.post_info a', this).text());
       var post_title = encodeURIComponent($('div.post_title', this).text());
-      var read_later_link = "\x3Ciframe border=\"0\" scrolling=\"no\" width=\"78\" height=\"17\" allowtransparency=\"true\" frameborder=\"0\"\n style=\"margin-bottom: -3px; z-index: 1338; border: 0px; background-color: transparent; overflow: hidden;\"\n src=\"http:\x2F\x2Fwww.instapaper.com\x2Fe2?url="+post_link+"\&title="+post_title+"\&description="+post_author+"\"\n\x3E\x3C\x2Fiframe\x3E";
+      var read_later_link = "\x3Ciframe border=\"0\" scrolling=\"no\" width=\"78\" height=\"17\" allowtransparency=\"true\" frameborder=\"0\"\n style=\"margin-bottom: -5px; z-index: 1338; border: 0px; background-color: transparent; overflow: hidden;\"\n src=\"http:\x2F\x2Fwww.instapaper.com\x2Fe2?url="+post_link+"\&title="+post_title+"\&description="+post_author+"\"\n\x3E\x3C\x2Fiframe\x3E";
     $('div.post_controls', this).prepend(read_later_link);
     }
   });
