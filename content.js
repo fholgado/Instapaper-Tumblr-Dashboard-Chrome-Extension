a3341d55f3f5bf8b45121e75f7179870a007adb6
Federico Holgado <fholgado@gmail.com>
2011-03-03 16:34:29 -0500

    Now adds "Read Later" button to picture posts > 1000 characters long

--- content.js
+++ content.js
@@ -14,5 +14,12 @@ $(document).ready(function(){
       read_later_link = "\x3Ciframe border=\"0\" scrolling=\"no\" width=\"78\" height=\"17\" allowtransparency=\"true\" frameborder=\"0\"\n style=\"margin-bottom: -5px; z-index: 1338; border: 0px; background-color: transparent; overflow: hidden;\"\n src=\"http:\x2F\x2Fwww.instapaper.com\x2Fe2?url="+post_link+"\&title="+post_title+"\&description="+post_author+"\"\n\x3E\x3C\x2Fiframe\x3E";
     $('div.post_controls', this).prepend(read_later_link);
     }
+    if($(this).hasClass('photo') && $('div.caption', this).text().length >= 1000){
+      post_link = encodeURIComponent($('a.permalink', this).attr('href'));
+      post_author = encodeURIComponent($('div.post_info a', this).text());
+      post_title = encodeURIComponent('Tumblr Photo post by ' + $('div.post_info a', this).text());
+      read_later_link = "\x3Ciframe border=\"0\" scrolling=\"no\" width=\"78\" height=\"17\" allowtransparency=\"true\" frameborder=\"0\"\n style=\"margin-bottom: -5px; z-index: 1338; border: 0px; background-color: transparent; overflow: hidden;\"\n src=\"http:\x2F\x2Fwww.instapaper.com\x2Fe2?url="+post_link+"\&title="+post_title+"\&description="+post_author+"\"\n\x3E\x3C\x2Fiframe\x3E";
+    $('div.post_controls', this).prepend(read_later_link);
+    }
   });
 });
